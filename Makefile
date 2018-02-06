b6fae7c3aacd1f2bc30dbd6d22e36ec0690bd1aa
Vikramjitsingh <300138075@AD226LNX02.ad.ufv.ca>
2018-02-05 16:10:09 -0800

    Started usign fseek

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 CC := gcc -Wall
 OPTS := -Wall -ggdb
 
-
+CFLAGS=-std=c99
 
 main:
-	$(CC) $(OPTS) $(LIBS) -o final main.c -pthread
+	$(CC) $(OPTS) $(LIBS) -o final main.c -pthread -std=c99
 
 clean:
 	@rm -f repl final
--- main.c
+++ main.c
@@ -1,105 +1,65 @@
-
-#include<stdio.h>
 #include<pthread.h>
 #include<stdlib.h>
 #include<string.h>
 #include<math.h>
+#include<stdio.h>
 #include<sys/stat.h>
 
-  int ssum = 0; // The final sum of all words
 
-  void *frequency(void *arg){
-  int *p = (int*) arg;
-  int limit = *p;
-  char filename[20];
-  sprintf(filename ,"%d" , limit);
-  char *back = ".txt";
-  strcat(filename,back);
-      FILE *ob = fopen(filename, "r");
-      int counter = 0;
-      while(!feof(ob)){
-      char c = getc(ob);
-      if(c == ' '){
-      counter++;
-      ssum++;
-  
-     }
-       }
-  if((limit == 1)|| (limit ==4)|| (limit == 16)){
-  printf("Filename %s  Number of Words %i \n " , filename, counter);
+int finalChars = 0;
+int fCounter = 0;
+
+void *counter(void *arg){
+fCounter++;
+
+int *ar = (int* ) arg;
+int numChars = *ar;
+int limit =0;
+limit = numChars*fCounter;
+
+
+int pre = limit - finalChars;
+
+FILE *ob = fopen("just.txt" , "r");
+
+printf("Start from %i \n" , pre);
+printf( "Upto %i \n" ,finalChars);
+
+
+fseek(ob ,  6 , SEEK_SET);
+
+for(int i=0; i<finalChars; i++){
+char c = getc(ob);
+printf("%c" , c);
 }
-           pthread_exit(0);
+pthread_exit(0);
 }
 
 
-int main(int adrgs, char **argv){
-       
-      
-
-      int segments = atoi(argv[1]); // Number files to break the main file into
+int main(){
+      int segments = 4; // Number files to break the main file into
       pthread_t tid[segments];
-      int args[segments];
- // It gives us the size of file total number of characters
+      int args;
+
+
        struct stat st;
        stat("just.txt", &st); 
        int size = st.st_size; // Number of Chars in main FILE
-       int numOfChars = size/segments; //Number of chars each newly file have
-// This loop will open  new files equal to required segments
-        for(int seg=0; seg<segments; seg++){ 
-        int pre = seg;     
-        char filename[100];
+       int numOfChars = size/segments;
+    printf(" Size of file %i \n " ,size);
+    printf( "Num chars %i \n " ,numOfChars);
+      
+       finalChars = numOfChars;
+      args = numOfChars;
+   for(int i=0; i<segments; i++){
+      
+   pthread_create(&tid[i], NULL, counter, &args);
 
-        sprintf(filename, "%d", pre);  // It converts integer into stirng
-        char *suf = ".txt";
-        strcat(filename, suf); //Combined filename
-        fopen(filename, "w"); // files opens here
- 
-}
-
-
-// Main potion of code
- FILE *mainFile;  // Main file of program
- mainFile  = fopen("just.txt", "r");
-
- FILE *openFile[segments]; // Array of File Pointers
-
- char *back = ".txt"; 
- // This open the already created file so that words can be appended in them
-  for(int fileNumber =0; fileNumber<segments; fileNumber++){
-  int front = fileNumber;
-  char filename1[100];
-  sprintf(filename1, "%d" , front);
-  strcat(filename1, back);
-
- openFile[fileNumber] = fopen(filename1,"a"); // Array pointers are used to create object of already created files
-}
-
- int checker = numOfChars;
- int controller =0;
- int fileCounter =0; // suffix of filename
- while(!feof(mainFile)){
-  char cc = getc(mainFile);
-  fputc(cc, openFile[fileCounter]);  //appending data into files
-  if(controller == checker){  // check if numchars reached
-  fclose(openFile[fileCounter]); // close previous file
-  fileCounter++;           // for new file
-  checker = checker+numOfChars; //new test range 
-  }
-
-  controller++;  // gets update each time
-
- }
-fclose(mainFile);
-// files have been written
-
-//Each thread will open different file
-     for( int test=0; test<segments; test++){
-     args[test] = test;
-     pthread_create(&tid[test], NULL , frequency, &args[test]);
-    }
-   for(int numjoin=0; numjoin<segments; numjoin++){
-    pthread_join(tid[numjoin], NULL);
    }
-   printf("Total Number of Words %d \n" , ssum);
-  exit(0);
+
+   for(int j=0; j<segments; j++){
+   pthread_join(tid[j], NULL);
+   }
+
+   exit(0);
 }
